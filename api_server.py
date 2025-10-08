8e9f1b4f0a573eabcef8dbaeb8825591dc701973
ThatGuySoham <sohamjagtap.engg2.4@gmail.com>
2025-10-08 17:26:10 +0530

    Update api_server.py

--- api_server.py
+++ api_server.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify, send_from_directory
 import json, os, time
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='dashboard') 
 DATA_PATH = os.path.join("dashboard", "data.json")
 
 @app.route('/')
 def serve_dashboard():
-    return send_from_directory("dashboard", "index.html")
+    dashboard_path = os.path.join(os.getcwd(), "dashboard")
+    return app.send_static_file('index.html') 
 
 @app.route('/update', methods=['POST'])
 def update_data():
@@ -25,6 +26,10 @@ def status():
         data = json.load(f)
     return jsonify(data)
 
+@app.route('/visuals/<path:traffic_bar_chart>')
+def serve_visuals(traffic_bar_chart):
+    return send_from_directory('dashboard/visuals', traffic_bar_chart)
+
 if __name__ == '__main__':
     print("🚦 Dashboard server running at http://127.0.0.1:5000/")
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
